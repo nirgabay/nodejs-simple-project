70c4222850ab539044eff39cb1c388ad030772bc
Nir Gabay <nir.gabay@gettacar.com>
2020-06-15 18:16:52 +0300

    fdsfds

--- git-pre-push-hook.sh
+++ git-pre-push-hook.sh
@@ -1,2 +1,9 @@
-echo 'Hello World'
-exit 1
+#!/bin/bash
+set -e
+
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+if [ "$branch" == "master" ]
+then
+    echo 'You cannot push to master dude'
+    exit 0
+fi
